42cc77dd08995fe416dbf310a87c44a3bc57283d
Soledium <nescobar3@hotmail.com>
2025-10-29 08:59:51 -0300

    Update settings.py

--- proyectoFirmas/settings.py
+++ proyectoFirmas/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'proyectoFirmas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_signapp',
-        'USER': 'postgres',      
-        'PASSWORD': 'ser.101990', 
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
