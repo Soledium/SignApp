4c67ab5ad2fa308538e228f9e00c484745579ad1
Soledium <nescobar3@hotmail.com>
2025-11-25 22:23:44 -0300

    Deploy fly.io
    Configuracion inicial para Fly.io y Whitenoise

--- proyectoFirmas/settings.py
+++ proyectoFirmas/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'proyectoFirmas.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3', # Se usa solo en local
+        conn_max_age=600
+    )
 }
 
 
